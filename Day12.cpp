ffb60c5de75f2d5bc961f9897e35f7c009514dd1
chan <thechanmoon@gmail.com>
2020-10-13 23:35:31 -0400

    Day12 cpp fixed

--- Day12.cpp
+++ Day12.cpp
@@ -74,3 +74,19 @@ class Student :  public Person{
 };
 
 int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
