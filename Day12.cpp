76c78a9efbd3762fe19daca228bdfacd702e53b6
chan <thechanmoon@gmail.com>
2020-10-13 23:37:20 -0400

    Day12 cpp test align

--- Day12.cpp
+++ Day12.cpp
@@ -35,42 +35,42 @@ class Student :  public Person{
         *   scores - An array of integers denoting the Person's test scores.
         */
         // Write your constructor here
-    Student(string firstName, string lastName, int identification, vector<int> testScores)  : Person(firstName, lastName, identification)
-    {
-        this->testScores = testScores;
-    }
-        /*	
-        *   Function Name: calculate
-        *   Return: A character denoting the grade.
-        */
-        // Write your function here
-    char calculate()
-    {
-        int sum = 0;
-        int avg = 0;
-        char grade = ' '; 
-        for(size_t i = 0; i < testScores.size(); i++)
+        Student(string firstName, string lastName, int identification, vector<int> testScores)  : Person(firstName, lastName, identification)
         {
-            sum += testScores[i];
+            this->testScores = testScores;
         }
-        if(testScores.size() != 0)
-            avg = sum/testScores.size();
+            /*	
+            *   Function Name: calculate
+            *   Return: A character denoting the grade.
+            */
+            // Write your function here
+        char calculate()
+        {
+            int sum = 0;
+            int avg = 0;
+            char grade = ' '; 
+            for(size_t i = 0; i < testScores.size(); i++)
+            {
+                sum += testScores[i];
+            }
+            if(testScores.size() != 0)
+                avg = sum/testScores.size();
 
-        if(avg >=90 && avg <=100)
-            grade = 'O';
-        else if(avg >=80 && avg <90)
-            grade = 'E';
-        else if(avg >=70 && avg <80)
-            grade = 'A';
-        else if(avg >=55 && avg <70)
-            grade = 'P';
-        else if(avg >=40 && avg <55)
-            grade = 'D';
-        else if(avg <40)
-            grade = 'T';
+            if(avg >=90 && avg <=100)
+                grade = 'O';
+            else if(avg >=80 && avg <90)
+                grade = 'E';
+            else if(avg >=70 && avg <80)
+                grade = 'A';
+            else if(avg >=55 && avg <70)
+                grade = 'P';
+            else if(avg >=40 && avg <55)
+                grade = 'D';
+            else if(avg <40)
+                grade = 'T';
 
-        return grade;
-    }
+            return grade;
+        }
 };
 
 int main() {
