9492defd262457943bfe2a72c66fd91c9bded126
chan <thechanmoon@gmail.com>
2020-11-15 00:57:35 -0500

    Day8 Create A Button

---
+++ 10DaysOfJavascript/Day8-CreateAButton/js/button.js
@@ -0,0 +1,9 @@
+let count = 0;  
+function myFunction() {
+  count = count + 1;  
+  document.getElementById("btn").innerHTML = count;
+}
+
+
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click",function() {btn.innerHTML++});
